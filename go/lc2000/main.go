9520e432ffa42f2dd320b31066bb93931675db0d
Runek00 <runek00@gmail.com>
2024-05-05 23:55:50 +0200

    missing package

--- go/lc2000/main.go
+++ go/lc2000/main.go
@@ -1,17 +1,21 @@
-func reversePrefix(word string, ch byte) string {
-    w := []byte(word)
-    j := 0
+package main
 
-    for k, c := range w {
-        if c == ch {
-            j = k
-            break
-        }
-    }
-    for i := 0; i <= j/2; i++ {
-        t := w[i]
-        w[i] = w[j-i]
-        w[j-i] = t
-    }
-    return string(w)
+func reversePrefix(word string, ch byte) string {
+	w := []byte(word)
+	j := 0
+
+	for k, c := range w {
+		if c == ch {
+			j = k
+			break
+		}
+	}
+	for i := 0; i <= j/2; i++ {
+		t := w[i]
+		w[i] = w[j-i]
+		w[j-i] = t
+	}
+	return string(w)
 }
+
+func main() {}
