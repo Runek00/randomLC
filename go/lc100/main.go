84333c0066adf6255248ef0aea30e8d23d727f17
Runek00 <runek00@gmail.com>
2024-05-08 21:45:13 +0200

    just an uncommited fix

--- go/lc100/main.go
+++ go/lc100/main.go
@@ -10,6 +10,9 @@ func isSameTree(p *TreeNode, q *TreeNode) bool {
 	if p == nil && q == nil {
 		return true
 	}
+	if p == nil || q == nil {
+		return false
+	}
 	if p.Val != q.Val {
 		return false
 	}
